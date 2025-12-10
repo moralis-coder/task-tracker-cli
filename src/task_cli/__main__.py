b7e7cc244b895db90f61ca9eae1b19f98707a904
moralis <moralis-coder@pm.me>
2025-12-10 21:17:39 +0100

    feat: add update task functionalities
    test plan:
    - try to update a non existing task
    - try to update a deleted task
    - try to mark in prorgress a task which is in already progress
    - try to mark in progresss a task which is done
    - try to mark done a task which is in todo
    - try to mark done a task which is done
    - mark in progress a task which is in todo
    - mark done a task which is in progress
    - update a task title which is not deleted

--- src/task_cli/__main__.py
+++ src/task_cli/__main__.py
@@ -7,6 +7,9 @@ from . import task
 COMMANDS = {
     'add': lambda **kwargs: commands.create_task(kwargs['task_name']),
     'delete': lambda **kwargs: commands.delete_task(kwargs['task_id']),
+    'update': lambda **kwargs: commands.update_task(kwargs['task_id'], kwargs['task_name']),
+    'mark-in-progress': lambda **kwargs: commands.mark_task_in_progress(kwargs['task_id']),
+    'mark-done': lambda **kwargs: commands.mark_task_done(kwargs['task_id']),
 }
 
 
